cb864b7e175a512388d32db98940197b1d79e6a4
lizewei <lizewei@iecas.cc>
2022-11-04 17:55:44 +0800

    add: 第三方包版本固化

--- utils/wechat_sdk/__init__.py
+++ utils/wechat_sdk/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from .login import WechatLogin
 
-appid = os.getenv('appid')
-app_secret = os.getenv('app_secret')
+appid = os.getenv('APPID')
+app_secret = os.getenv('APP_SECRET')
 
 wechat_login = WechatLogin(appid, app_secret)
