cfb59ceccf4fe75f6255569088202d8844cfa3a1
lizewei <lizewei@iecas.cc>
2022-11-22 10:00:37 +0800

    add: go-fastdfs服务

--- apps/upload_file/views.py
+++ apps/upload_file/views.py
@@ -12,14 +12,23 @@ from utils.response import success
 
 
 class UploadAuthView(APIView):
-    """
-        go-fastdfs文件上传认证
-        https://sjqzhang.github.io/go-fastdfs/authentication.html#custom
-    """
+
     def post(self, request, *args, **kwargs):
+        """
+            go-fastdfs文件上传认证
+            https://sjqzhang.github.io/go-fastdfs/authentication.html#custom
+
+            在go-fastdfs配置文件中配置`auth_url`，/file_service/upload_auth。
+            前端会携带`auth_token`凭证请求是否允许上传文件
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         code = request.data.get('auth_token')
         redis_conn = get_redis_connection('valid_auth')
         if redis_conn.get(code):
+            redis_conn.delete(code)
             return HttpResponse("ok")
         else:
             return HttpResponse("fail")
@@ -33,6 +42,6 @@ class AuthTokenView(APIView):
         code = str(uuid.uuid4()).replace('-', '')
 
         redis_conn = get_redis_connection('valid_auth')
-        redis_conn.setex(code, 60 * 5, request.user.id)
+        redis_conn.setex(code, 60 * 30, request.user.id)
         return success({"code": code})
 
